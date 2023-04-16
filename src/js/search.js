6f0a9fc8e95da017f0f26c6da7b81b76f9c33f00
Wiktor Kozłowski <kozlowskiwiktor92@gmail.com>
2023-04-16 12:39:45 +0200

    First part of search.js
    added qs for form__search
    added searchParams
    added addEventListener

---
+++ src/js/search.js
@@ -0,0 +1,31 @@
+import api from "./api";
+import axios from "axios";
+axios.defaults.baseURL = api.BASE_URL;
+
+const searchForm = document.querySelector("#form__search");
+const inputDOM = document.querySelector(".form__search--input")
+
+const searchParams = {
+    api_key: api.API_KEY,
+    query: '',
+    page: 1,
+    per_page: 40,
+}
+
+searchForm.addEventListener("submit", async event => {
+    event.preventDefault();
+
+    const textInput = inputDOM.value;
+    searchParams.query = textInput
+
+    page = 1;
+
+    try {
+        const response = await axios.get("/search/movie", {
+            params: searchParams,
+        })
+        console.log(response)
+    } catch (error) {
+        Notiflix.Notify.failure(`Sorry, there are no movie matching your search query. Please try again.`)
+    }
+});
