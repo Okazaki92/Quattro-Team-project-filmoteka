4c3e42b0e594a222d9a378788f1041c8ba4a15fd
Wiktor Kozłowski <kozlowskiwiktor92@gmail.com>
2023-04-16 21:24:54 +0200

    Imported function renderTrendingGallery
    Imported and used function.
    Added moviesList.innerHTML
    Deleted "per_page: 40" in searchParams

--- src/js/search.js
+++ src/js/search.js
@@ -1,31 +1,38 @@
 import api from "./api";
 import axios from "axios";
+import tredingGallery from "./renderGallery";
 axios.defaults.baseURL = api.BASE_URL;
 
 const searchForm = document.querySelector("#form__search");
 const inputDOM = document.querySelector(".form__search--input")
+const moviesList = document.querySelector(".movies__list");
 
 const searchParams = {
     api_key: api.API_KEY,
     query: '',
     page: 1,
-    per_page: 40,
 }
 
 searchForm.addEventListener("submit", async event => {
     event.preventDefault();
-
+    
     const textInput = inputDOM.value;
     searchParams.query = textInput
-
+    
     page = 1;
-
+    
     try {
         const response = await axios.get("/search/movie", {
             params: searchParams,
         })
-        console.log(response)
+        console.log(response);
+        
+        const searchedMovies = await response.data.results
+        console.log(searchedMovies)
+       
+        tredingGallery.renderTrendingGallery(searchedMovies);
+        moviesList.innerHTML = "";
     } catch (error) {
-        Notiflix.Notify.failure(`Sorry, there are no movie matching your search query. Please try again.`)
+        console.error(error)
     }
 });
