d187eeb470d319ea5ad6b49618991dc7c39ce9e8
Wiktor Kozłowski <kozlowskiwiktor92@gmail.com>
2023-04-16 21:22:52 +0200

    Added "?" to .slice()
    Added "?" before .slice() in function renderTrendingGallery

--- src/js/renderGallery.js
+++ src/js/renderGallery.js
@@ -28,7 +28,7 @@ const renderTrendingGallery = async (movies) => {
           <h3 class="movie__title">${title || name}</h3>
           <div class="movie__subtitle">
           <p class="movie__genre">${genreNames}</p>
-          <p class="movie__date">${(release_date || first_air_date).slice(0, 4)}</p>
+          <p class="movie__date">${(release_date || first_air_date)?.slice(0, 4)}</p>
           <p class="movie__rating">${vote_average.toFixed(2) || "-"}</p>
           </div>
         </div>
