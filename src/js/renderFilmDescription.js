38a82a240058e06460b16423c390641405d0c389
jaworskiwojciech <jaworski.wojciech94@gmail.com>
2023-04-17 23:43:25 +0200

    Naprawiono html i style

--- src/js/renderFilmDescription.js
+++ src/js/renderFilmDescription.js
@@ -1,29 +1,33 @@
-const renderDescription = (movie) => {
-	const genre = movie.genres.map(({ name }) => name).join(", ");
-	return `
+const renderDescription = movie => {
+  const genre = movie.genres.map(({ name }) => name).join(', ');
+  return `
             <img class="modal__image" src=https://image.tmdb.org/t/p/original${
-							movie.poster_path
-						} alt="${movie.title || movie.name}">
+              movie.poster_path
+            } alt="${movie.title || movie.name}">
             <div class="modal__textbox">
     <h2 class="modal__header">${movie.title || movie.name}</h2>
     <div class="modal__features">
       <div class="modal__features--keys">
-        <p>Vote / Votes</p>
-        <p>Popularity</p>
-        <p>Original Title</p>
-        <p>Genre</p>
+        <p class="modal__feature">Vote / Votes</p>
+        <p class="modal__feature">Popularity</p>
+        <p class="modal__feature">Original Title</p>
+        <p class="modal__feature">Genre</p>
       </div>
       <div class="modal__features--values">
-        <p><span>${movie.vote_average}</span> / ${movie.vote_count}</p>
-        <p>${movie.popularity}</p>
-        <p>${movie.original_title || movie.original_name}</p>
-        <p>${genre}</p>
+        <p class="modal__feature"><button>${movie.vote_average}</button> / ${movie.vote_count}</p>
+        <p class="modal__feature">${movie.popularity}</p>
+        <p class="modal__feature">${movie.original_title || movie.original_name}</p>
+        <p class="modal__feature">${genre}</p>
       </div>
     </div>
     <div class="modal__description-container">
       <h3 class="modal__description-heading">ABOUT</h3>
       <p class="modal__description-text">${movie.overview}</p>
     </div>
+    <div class="modal__buttons">
+        <button class="modal__buttons--watched">ADD TO WATCHED</button
+        ><button class="modal__buttons--queue">ADD TO QUEUE</button>
+      </div>
   </div>`;
 };
 const renderFilmDescription = { renderDescription };
