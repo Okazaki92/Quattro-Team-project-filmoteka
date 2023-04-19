22f5365ea6b33edd49aca243cf973a0128c281ba
Okazaki92 <vincentslominski@gmail.com>
2023-04-19 22:19:34 +0200

    changed movie.vote to fixed 1, added pagination lib

--- src/js/renderFilmDescription.js
+++ src/js/renderFilmDescription.js
@@ -14,7 +14,7 @@ const renderDescription = (movie) => {
         <p class="modal__feature">Genre</p>
       </div>
       <div class="modal__features--values">
-      <p class="modal__feature"><button>${movie.vote_average.toFixed(2)}</button> / ${movie.vote_count}</p>
+      <p class="modal__feature"><button>${movie.vote_average.toFixed(1)}</button> / ${movie.vote_count}</p>
         <p class="modal__feature">${movie.popularity}</p>
         <p class="modal__feature">${movie.original_title || movie.original_name}</p>
         <p class="modal__feature">${genre}</p>
