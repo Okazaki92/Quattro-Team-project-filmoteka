c89c99a7b70ec6d2e1d7ea3079fc9715655da041
SzymonSleboda <szymonskeboda@gmail.com>
2023-04-20 17:55:55 +0200

    feature/added modal animations v1 (will be refined)

--- src/js/modal.js
+++ src/js/modal.js
@@ -1,42 +1,44 @@
-import getFilmDescription from "./getFilmDescription";
-import renderFilmDescription from "./renderFilmDescription";
-const modalDOM = document.querySelector(".modal__window");
+import getFilmDescription from './getFilmDescription';
+import renderFilmDescription from './renderFilmDescription';
+const modalDOM = document.querySelector('.modal__window');
 
-const qs = (e) => document.querySelector(e);
+const qs = e => document.querySelector(e);
 
-const closeBtn = qs("#modal__close");
-const imageItem = qs(".movies__list");
-const modal = qs(".modal");
+const closeBtn = qs('#modal__close');
+const imageItem = qs('.movies__list');
+const modal = qs('.modal');
 
-const openModal = async (e) => {
-	e.preventDefault();
-	let movieId = 0;
-	if (!e.target.closest("li")) {
-		return;
-	}
-	modal.classList.remove("is-hidden");
-	modal.classList.add("is-loading");
-	movieId = e.target.closest("li").dataset.id;
-	try {
-		const data = await getFilmDescription.getMovieDescription(movieId);
-		modalDOM.insertAdjacentHTML(
-			"beforeend",
-			renderFilmDescription.renderDescription(data),
-		);
-	} catch (error) {
-		console.log(error);
-	}
-	finally {
-        modal.classList.remove("is-loading");
-    }
+const openModal = async e => {
+  e.preventDefault();
+  if (modal.classList.contains('is-hidden') === false) {
+    return;
+  }
+  let movieId = 0;
+  if (!e.target.closest('li')) {
+    return;
+  }
+  modal.classList.remove('is-hidden');
+  modal.classList.add('is-loading', 'modal__window--open');
+  movieId = e.target.closest('li').dataset.id;
+  try {
+    const data = await getFilmDescription.getMovieDescription(movieId);
+    modalDOM.insertAdjacentHTML('beforeend', renderFilmDescription.renderDescription(data));
+  } catch (error) {
+    console.log(error);
+  } finally {
+    modal.classList.remove('is-loading');
+  }
 };
-
 const closeModal = () => {
-	modal.classList.add("is-hidden");
-	const modalImg = qs(".modal__image");
-	const modalTextbox = qs(".modal__textbox");
-	modalTextbox.remove();
-	modalImg.remove();
+  modal.classList.add('modal__window--close');
+  setTimeout(() => {
+    modal.classList.add('is-hidden');
+    modal.classList.remove('modal__window--close');
+    const modalImg = qs('.modal__image');
+    const modalTextbox = qs('.modal__textbox');
+    modalTextbox.remove();
+    modalImg.remove();
+  }, 500);
 };
-closeBtn.addEventListener("click", closeModal);
-imageItem.addEventListener("click", openModal);
+closeBtn.addEventListener('click', closeModal);
+imageItem.addEventListener('click',openModal);
