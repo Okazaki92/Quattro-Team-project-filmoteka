008d7d525729dc7153217bc128e70f4a54a674dc
Okazaki92 <vincentslominski@gmail.com>
2023-04-21 19:22:55 +0200

    minor code refactor at renderGallery and pagination

--- src/js/pagination.js
+++ src/js/pagination.js
@@ -1,30 +1,29 @@
-import getGallery from './getGallery';
-import tredingGallery from './renderGallery';
-import Pagination from 'tui-pagination';
-import 'tui-pagination/dist/tui-pagination.css';
+import getGallery from "./getGallery";
+import tredingGallery from "./renderGallery";
+import Pagination from "tui-pagination";
+import "tui-pagination/dist/tui-pagination.css";
 
-const paginationDOM = document.querySelector('#pagination');
+const paginationDOM = document.querySelector("#pagination");
 
-const setPagination = async event => {
-  page = 1;
-  try {
-    const data = await getGallery.getTrendingGallery(page);
-    const options = {
-      totalItems: data.total_results,
-      itemsPerPage: 20,
-      visiblePages: 5,
-      page: 1,
-      centerAlign: true,
-    };
+const setPagination = async (event) => {
+	try {
+		const data = await getGallery.getTrendingGallery(page);
+		const options = {
+			totalItems: data.total_results,
+			itemsPerPage: 20,
+			visiblePages: 5,
+			page: 1,
+			centerAlign: true,
+		};
 
-    const pagination = new Pagination(paginationDOM, options);
+		const pagination = new Pagination(paginationDOM, options);
 
-    pagination.on('beforeMove', async event => {
-      const newPageData = await getGallery.getTrendingGallery(event.page);
-      tredingGallery.renderTrendingGallery(newPageData.results);
-    });
-  } catch (error) {
-    console.log(error);
-  }
+		pagination.on("beforeMove", async (event) => {
+			const newPageData = await getGallery.getTrendingGallery(event.page);
+			tredingGallery.renderTrendingGallery(newPageData.results);
+		});
+	} catch (error) {
+		console.log(error);
+	}
 };
 export default setPagination;
