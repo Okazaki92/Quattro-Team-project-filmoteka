8acc24c5629f89de8940c2d6d556c02f0bff8ae7
Sylvia Stachera <sylwia.stachera4@gmail.com>
2023-04-21 18:20:57 +0200

    Clean code and fix btn in headerLibrary.js

--- src/js/pagination.js
+++ src/js/pagination.js
@@ -1,31 +1,30 @@
-import getGallery from "./getGallery";
-import tredingGallery from "./renderGallery";
-import Pagination from "tui-pagination";
-import "tui-pagination/dist/tui-pagination.css";
+import getGallery from './getGallery';
+import tredingGallery from './renderGallery';
+import Pagination from 'tui-pagination';
+import 'tui-pagination/dist/tui-pagination.css';
 
-const paginationDOM = document.querySelector("#pagination");
+const paginationDOM = document.querySelector('#pagination');
 
-const setPagination = async (event) => {
-	page = 1;
-	try {
-		const data = await getGallery.getTrendingGallery(page);
-		console.log(data);
-		const options = {
-			totalItems: data.total_results,
-			itemsPerPage: 20,
-			visiblePages: 5,
-			page: 1,
-			centerAlign: true,
-		};
-		console.log(options);
-		const pagination = new Pagination(paginationDOM, options);
+const setPagination = async event => {
+  page = 1;
+  try {
+    const data = await getGallery.getTrendingGallery(page);
+    const options = {
+      totalItems: data.total_results,
+      itemsPerPage: 20,
+      visiblePages: 5,
+      page: 1,
+      centerAlign: true,
+    };
 
-		pagination.on("beforeMove", async (event) => {
-			const newPageData = await getGallery.getTrendingGallery(event.page);
-			tredingGallery.renderTrendingGallery(newPageData.results);
-		});
-	} catch (error) {
-		console.log(error);
-	}
+    const pagination = new Pagination(paginationDOM, options);
+
+    pagination.on('beforeMove', async event => {
+      const newPageData = await getGallery.getTrendingGallery(event.page);
+      tredingGallery.renderTrendingGallery(newPageData.results);
+    });
+  } catch (error) {
+    console.log(error);
+  }
 };
 export default setPagination;
