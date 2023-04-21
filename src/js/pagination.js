116b95b978d325cabc6c594eb6310f87079fec25
Sylvia Stachera <sylwia.stachera4@gmail.com>
2023-04-21 17:55:09 +0200

    Clean code pagination.js

--- src/js/pagination.js
+++ src/js/pagination.js
@@ -3,36 +3,7 @@ import tredingGallery from "./renderGallery";
 import Pagination from "tui-pagination";
 import "tui-pagination/dist/tui-pagination.css";
 
-const moviesDOM = document.querySelector(".movies__list");
 const paginationDOM = document.querySelector("#pagination");
-
-// const setPagination = totalItems => {
-//   const options = {
-//     totalItems,
-//     itemsPerPage: 10,
-//     visiblePages: 5,
-//     centerAlign: true,
-//   };
-
-//   const pagination = new Pagination(paginationDOM, options);
-
-//   return pagination;
-// };
-
-// const getPagination = async () => {
-//   try {
-//     const data = await getGallery.getTrendingGallery(1);
-//     const pagination = setPagination(data.totalItems);
-
-//     pagination.on('beforeMove', ({ page }) => {
-//       tredingGallery.renderTrendingGallery(page);
-//     });
-//   } catch (error) {
-//     console.error(error);
-//   }
-// };
-
-// export default getPagination;
 
 const setPagination = async (event) => {
 	page = 1;
