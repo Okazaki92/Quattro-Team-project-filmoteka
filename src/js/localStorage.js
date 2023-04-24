83fffbb5e2752bf16e45e13f3fb09d2bdd5d7761
Piotr Baraniak <piterb86@gmail.com>
2023-04-24 19:59:05 +0200

    workingPaginationLibrary ?
    chyba będzie działać

--- src/js/localStorage.js
+++ src/js/localStorage.js
@@ -1,40 +1,42 @@
 const addToLocalStorage = (key, value) => {
-	const data = JSON.parse(localStorage.getItem(key)) || [];
-	data.push(value);
-	localStorage.setItem(key, JSON.stringify(data));
+  const data = JSON.parse(localStorage.getItem(key)) || [];
+  data.push(value);
+  localStorage.setItem(key, JSON.stringify(data));
 };
 
 export const addToWatched = (movie) => {
-	addToLocalStorage("watched", movie);
+  addToLocalStorage("watched", movie);
 };
 
 export const addToQueue = (movie) => {
-	addToLocalStorage("queue", movie);
+  addToLocalStorage("queue", movie);
 };
 
 export const isMovieInList = (key, movieId) => {
-	const data = JSON.parse(localStorage.getItem(key)) || [];
-	return data.some((movie) => movie.id === movieId);
+  const data = JSON.parse(localStorage.getItem(key)) || [];
+  return data.some((movie) => movie.id === movieId);
 };
 
 export const removeFromList = (key, movieId) => {
-	const data = JSON.parse(localStorage.getItem(key)) || [];
-	const updatedData = data.filter((movie) => movie.id !== movieId);
-	localStorage.setItem(key, JSON.stringify(updatedData));
-};
-const getFromLocalStorage = (key, value) => {
-	const data = JSON.parse(localStorage.getItem(key));
-	console.log(data);
-	return data;
+  const data = JSON.parse(localStorage.getItem(key)) || [];
+  const updatedData = data.filter((movie) => movie.id !== movieId);
+  localStorage.setItem(key, JSON.stringify(updatedData));
 };
 
-export const getFromWatched = (movie) => {
-	const data = getFromLocalStorage("watched");
-	console.log(data);
-	return data;
+const getFromLocalStorage = (key, page) => {
+  const data = JSON.parse(localStorage.getItem(key));
+  const itemsPerPage = 15;
+  const start = (page - 1) * itemsPerPage;
+  const end = start + itemsPerPage;
+  return data.slice(start, end);
 };
 
-export const getFromQueued = (movie) => {
-	const data = getFromLocalStorage("queue");
-	return data;
+export const getFromWatched = (page) => {
+  const data = getFromLocalStorage("watched", page);
+  return data;
+};
+
+export const getFromQueued = (page) => {
+  const data = getFromLocalStorage("queue", page);
+  return data;
 };
--- src/js/renderLibraryMovies.js
+++ src/js/renderLibraryMovies.js
@@ -4,65 +4,65 @@ const moviesDOM = document.querySelector(".movies__list");
 const IMG_URL = "https://image.tmdb.org/t/p/original";
 const IMG_URL_DEFAULT = "https://i.ibb.co/xq4LQMw/Filmoteka-by-Quattro.jpg";
 
-export const renderWatchedMovies = async (movies) => {
-	let genreNames = [];
-	const genres = await getGallery.getGalleryGenres();
-	console.log(movies);
-	const markup = movies
-		.map(
-			({
-				title,
-				poster_path,
-				release_date,
-				first_air_date,
-				name,
-				vote_average,
-				genre_ids,
-				id,
-			}) => {
-				if (genre_ids) {
-					genreNames = genres
-						.filter(({ id }) => genre_ids.includes(id))
-						.map(({ name }) => name)
-						.join(", ");
-				}
-				return `
+export const renderLibraryMovies = async (movies) => {
+  let genreNames = [];
+  const genres = await getGallery.getGalleryGenres();
+  console.log(movies);
+  const markup = movies
+    .map(
+      ({
+        title,
+        poster_path,
+        release_date,
+        first_air_date,
+        name,
+        vote_average,
+        genre_ids,
+        id,
+      }) => {
+        if (genre_ids) {
+          genreNames = genres
+            .filter(({ id }) => genre_ids.includes(id))
+            .map(({ name }) => name)
+            .join(", ");
+        }
+        return `
             <li class="movie" data-id=${id}>
             <img class="movie__image" src="${
-							poster_path ? IMG_URL + poster_path : IMG_URL_DEFAULT
-						}" alt="${title || name}" loading="lazy">
+              poster_path ? IMG_URL + poster_path : IMG_URL_DEFAULT
+            }" alt="${title || name}" loading="lazy">
             <div class="movie__textbox">
           <h3 class="movie__title">${title || name}</h3>
           <div class="movie__subtitle">
           <span class="movie__genre">${genreNames}</span>
           <span class="movie__date">| ${
-						(release_date || first_air_date)?.slice(0, 4) || "-"
-					}</span>
+            (release_date || first_air_date)?.slice(0, 4) || "-"
+          }</span>
           <span class="movie__rating">| ${vote_average.toFixed(1) || "-"}</span>
           </div>
         </div>
             </li>`;
-			},
-		)
-		.join("");
-	moviesDOM.insertAdjacentHTML("beforeend", markup);
+      },
+    )
+    .join("");
+  moviesDOM.innerHTML = '';
+  moviesDOM.insertAdjacentHTML("beforeend", markup);
 };
 
 export const submitWatched = (event) => {
-	moviesDOM.innerHTML = "";
-	try {
-		const data = getFromWatched();
-		renderWatchedMovies(data);
-	} catch (error) {
-		console.log(error);
-	}
+  try {
+    const data = getFromWatched(1);
+    renderLibraryMovies(data);
+  } catch (error) {
+    console.log(error);
+  }
 };
+
 export const submitQueued = (event) => {
-	moviesDOM.innerHTML = "";
-	try {
-		const data = getFromQueued();
-		renderWatchedMovies(data);
-	} catch (error) {
-		console.log(error);
-	}
+  try {
+    const data = getFromQueued(1);
+    renderLibraryMovies(data);
+  } catch (error) {
+    console.log(error);
+  }
 };
