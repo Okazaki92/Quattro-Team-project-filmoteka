a844d42e92167d7dd494801ddc4028b3ea773e50
Okazaki92 <vincentslominski@gmail.com>
2023-06-11 11:37:20 +0200

    added env API_KEY

--- src/js/api.js
+++ src/js/api.js
@@ -1,5 +1,5 @@
-const API_KEY = "70ee022622d62c8aef7c9ed9d4172d5b";
-const BASE_URL = "https://api.themoviedb.org/3/";
+const API_KEY = process.env.API_LIBRARY_KEY;
+const BASE_URL = 'https://api.themoviedb.org/3/';
 
 const api = { API_KEY, BASE_URL };
 export default api;
