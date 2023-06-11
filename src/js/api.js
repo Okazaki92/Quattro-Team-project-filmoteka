5c334bba0472c6e7ea650cac9f8a84d0f1b8d385
Okazaki92 <vincentslominski@gmail.com>
2023-06-11 12:04:33 +0200

    fix/return API

--- src/js/api.js
+++ src/js/api.js
@@ -1,7 +1,5 @@
-require('dotenv').config();
-
-const API_KEY = process.env.API_LIBRARY_KEY;
-const BASE_URL = 'https://api.themoviedb.org/3/';
+const API_KEY = "70ee022622d62c8aef7c9ed9d4172d5b";
+const BASE_URL = "https://api.themoviedb.org/3/";
 
 const api = { API_KEY, BASE_URL };
 export default api;
